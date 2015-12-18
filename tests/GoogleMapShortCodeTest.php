12e7c92ebd9b65a30704b5f9337e0330c4f9ccd6
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-18 21:46:47 +0700

    FIX: Use the reset counter trick as per streetview

--- tests/GoogleMapShortCodeTest.php
+++ tests/GoogleMapShortCodeTest.php
@@ -8,13 +8,14 @@ class GoogleMapShortCodeTest extends SapphireTest
 	protected static $fixture_file = 'mappable/tests/shortcodes.yml';
 
 	public function testRoadMap() {
+		GoogleMapShortCodeHandler::resetCounter();
 		$page = $this->objFromFixture('Page', 'RoadMap');
 		$html = ShortcodeParser::get_active()->parse($page->Content);
 		$expected = <<<TEXT
 Some text
 
 <div class="googlemapcontainer">
-<div id="google_sc_map_9" class="map googlemap" data-shortcode-map="" data-latitude="13.7402946" data-longitude="100.5525439" data-zoom="14" data-maptype="road" data-allowfullscreen="1"></div>
+<div id="google_sc_map_1" class="map googlemap" data-shortcode-map="" data-latitude="13.7402946" data-longitude="100.5525439" data-zoom="14" data-maptype="road" data-allowfullscreen="1"></div>
 <p class="caption">Roads in Central Bangkok</p>
 </div>
 
@@ -24,13 +25,14 @@ TEXT;
 
 
 	public function testAerialMap() {
+		GoogleMapShortCodeHandler::resetCounter();
 		$page = $this->objFromFixture('Page', 'AerialMap');
 		$html = ShortcodeParser::get_active()->parse($page->Content);
 		$expected = <<<TEXT
 Some text
 
 <div class="googlemapcontainer">
-<div id="google_sc_map_18" class="map googlemap" data-shortcode-map="" data-latitude="13.815483" data-longitude="100.5447213" data-zoom="20" data-maptype="aerial" data-allowfullscreen="1"></div>
+<div id="google_sc_map_1" class="map googlemap" data-shortcode-map="" data-latitude="13.815483" data-longitude="100.5447213" data-zoom="20" data-maptype="aerial" data-allowfullscreen="1"></div>
 <p class="caption">Bang Sue Train Depot, Thailand</p>
 </div>
 
@@ -40,13 +42,14 @@ TEXT;
 
 
 	public function testHybridMap() {
+		GoogleMapShortCodeHandler::resetCounter();
 		$page = $this->objFromFixture('Page', 'HybridMap');
 		$html = ShortcodeParser::get_active()->parse($page->Content);
 		$expected = <<<TEXT
 Some text
 
 <div class="googlemapcontainer">
-<div id="google_sc_map_27" class="map googlemap" data-shortcode-map="" data-latitude="13.8309545" data-longitude="100.5577219" data-zoom="18" data-maptype="hybrid" data-allowfullscreen="1"></div>
+<div id="google_sc_map_1" class="map googlemap" data-shortcode-map="" data-latitude="13.8309545" data-longitude="100.5577219" data-zoom="18" data-maptype="hybrid" data-allowfullscreen="1"></div>
 <p class="caption">Junction in Bangkok, Thailand</p>
 </div>
 
@@ -56,13 +59,14 @@ TEXT;
 
 
 	public function testTerrainmap() {
+		GoogleMapShortCodeHandler::resetCounter();
 		$page = $this->objFromFixture('Page', 'TerrainMap');
 		$html = ShortcodeParser::get_active()->parse($page->Content);
 		$expected = <<<TEXT
 Some text
 
 <div class="googlemapcontainer">
-<div id="google_sc_map_36" class="map googlemap" data-shortcode-map="" data-latitude="18.8032393" data-longitude="98.9166518" data-zoom="14" data-maptype="terrain" data-allowfullscreen="1"></div>
+<div id="google_sc_map_1" class="map googlemap" data-shortcode-map="" data-latitude="18.8032393" data-longitude="98.9166518" data-zoom="14" data-maptype="terrain" data-allowfullscreen="1"></div>
 <p class="caption">Mountains west of Chiang Mai</p>
 </div>
 
