a0e4cd1aa8a1ca9236c1eebd55db060c50940ff1
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-23 01:19:25 +0700

    FIX: Add longer cache for Google geocoding results

--- _config.php
+++ _config.php
@@ -8,3 +8,6 @@ if(!defined('MAPPABLE_MODULE_PATH'))
 
 ShortcodeParser::get('default')->register('GoogleStreetView',array('GoogleStreetViewShortCodeHandler','parse_googlestreetview'));
 ShortcodeParser::get('default')->register('GoogleMap',array('GoogleMapShortCodeHandler','parse_googlemap'));
+
+// recommended update time is 20 mins so play safe and go with 30.
+SS_Cache::set_cache_lifetime('mappablegeocoder', 30*60);
