ea4cb70108f4c2000a7e9b246eb84e82c016f437
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-23 14:20:58 +0700

    ENHANCEMENT: Test static map with a pin

--- tests/MappableDataTest.php
+++ tests/MappableDataTest.php
@@ -142,7 +142,6 @@ HTML;
 		$this->assertEquals($expected, $html);
 	}
 
-
 	public function testStaticMapVaryZoom() {
 		$instance = $this->getInstance();
 
@@ -161,6 +160,21 @@ HTML;
 		$this->assertEquals($expected, $html);
 	}
 
+	public function testStaticMapWithMapPin() {
+		$instance = $this->getInstance();
+		$this->addMapPinToInstance($instance);
+
+		$html = $instance->StaticMap(300, 800, 2);
+		$expected = '<img src="//maps.googleapis.com/maps/api/staticmap?center='
+				  . '13.8188931%2C100.5005558&amp;markers=icon%3Ahttp%3A%2F%2Fd'
+				  . 'ev.jakayanrides.com%2Fmappable%2Ftests%2Fimages%2Fmapicont'
+				  . 'est.png%7C13.8188931%2C100.5005558&amp;zoom=2&amp;size=300'
+				  . 'x800&amp;sensor=false&amp;maptype=roadmap" width="300" hei'
+				  . 'ght="800" alt="User, Test" />';
+		$this->assertEquals($expected, $html);
+
+	}
+
 	private function getInstance() {
 		$instance = new Member();
 		$instance->Lat = 13.8188931;
@@ -168,6 +182,15 @@ HTML;
 		$instance->FirstName = 'Test';
 		$instance->Surname = 'User';
 		return $instance;
+	}
+
+
+	private function addMapPinToInstance(&$instance) {
+		// Create a pin
+		$imageFile = new Image(array('Name' => 'mapicontest.png', 'Filename' => 'mappable/tests/images/mapicontest.png'));
+		$imageFile->write();
+		$instance->MapPinIconID = $imageFile->ID;
+		$instance->write();
 	}
 
 }
