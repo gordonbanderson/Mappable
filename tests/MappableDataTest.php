b615bc3fb25dd515ea8167b70e23aed2d95c6074
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-25 16:31:23 +0700

    FIX: Fix for map icon moving

--- tests/MappableDataTest.php
+++ tests/MappableDataTest.php
@@ -171,12 +171,7 @@ HTML;
 		$this->addMapPinToInstance($instance);
 
 		$html = $instance->StaticMap(300, 800, 2);
-		$expected = '<img src="//maps.googleapis.com/maps/api/staticmap?center='
-				  . '13.8188931%2C100.5005558&amp;markers=icon%3Ahttp%3A%2F%2Fd'
-				  . 'ev.jakayanrides.com%2Fmappable%2Ftests%2Fimages%2Fmapicont'
-				  . 'est.png%7C13.8188931%2C100.5005558&amp;zoom=2&amp;size=300'
-				  . 'x800&amp;sensor=false&amp;maptype=roadmap" width="300" hei'
-				  . 'ght="800" alt="User, Test" />';
+		$expected = '<img src="//maps.googleapis.com/maps/api/staticmap?center=13.8188931%2C100.5005558&amp;markers=icon%3Ahttp%3A%2F%2Fdev.jakayanrides.com%2Fassets%2Fmapicontest.png%7C13.8188931%2C100.5005558&amp;zoom=2&amp;size=300x800&amp;sensor=false&amp;maptype=roadmap" width="300" height="800" alt="User, Test" />';
 		$this->assertEquals($expected, $html);
 
 	}
@@ -196,7 +191,9 @@ HTML;
 	 */
 	private function addMapPinToInstance(&$instance) {
 		// Create a pin
-		$imageFile = new Image(array('Name' => 'mapicontest.png', 'Filename' => 'mappable/tests/images/mapicontest.png'));
+		$copied = copy('mappable/tests/images/mapicontest.png', 'assets/mapicontest.png');
+		$this->assertTrue($copied);
+		$imageFile = new Image(array('Name' => 'mapicontest.png', 'Filename' => 'assets/mapicontest.png'));
 		$imageFile->write();
 		$instance->MapPinIconID = $imageFile->ID;
 		$instance->write();
