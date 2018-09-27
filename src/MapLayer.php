c4336b05430a2e23e8216b80b80b161b68cdf688
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-09-27 19:26:43 +0100

    FIX: Table name

--- src/MapLayer.php
+++ src/MapLayer.php
@@ -5,6 +5,8 @@ use SilverStripe\ORM\DataObject;
 
 class MapLayer extends DataObject
 {
+    private static $table_name = 'MapLayer';
+
     private static $db = array(
         'Title' => 'Varchar(255)',
     );
